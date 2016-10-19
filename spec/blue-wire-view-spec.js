5f1833dfb7814b4e02db24bce636309637537d1c
quantifiedtran <mari.harald.u.k@gmail.com>
2016-10-19 15:08:45 +0100

    Removed unnecessary files

--- spec/blue-wire-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import BlueWireView from '../lib/blue-wire-view';
-
-describe('BlueWireView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
